de385133e5e71fce0b9db809d57e567f01d14ab2
Martin Graversen <martin@graversen.io>
2019-03-17 15:36:59 +0100

    :tada: storage-app :tada: worker-app

---
+++ storage-app/src/main/java/io/graversen/distributed/hashing/storage/app/StorageApp.java
@@ -0,0 +1,5 @@
+package io.graversen.distributed.hashing.storage.app;
+
+public class StorageApp
+{
+}
---
+++ worker-app/src/main/java/io/graversen/distributed/hashing/worker/app/WorkerApp.java
@@ -0,0 +1,5 @@
+package io.graversen.distributed.hashing.worker.app;
+
+public class WorkerApp
+{
+}

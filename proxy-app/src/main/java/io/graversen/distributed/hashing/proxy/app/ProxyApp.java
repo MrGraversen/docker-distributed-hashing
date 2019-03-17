31aef918f67caa6c1e152ef99e07d2de70b37c71
Martin Graversen <martin@graversen.io>
2019-03-17 18:38:25 +0100

    Stand up proxy-app

--- proxy-app/src/main/java/io/graversen/distributed/hashing/proxy/app/ProxyApp.java
+++ proxy-app/src/main/java/io/graversen/distributed/hashing/proxy/app/ProxyApp.java
@@ -1,5 +1,17 @@
 package io.graversen.distributed.hashing.proxy.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
 public class ProxyApp
 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProxyApp.class, args);
+    }
 }

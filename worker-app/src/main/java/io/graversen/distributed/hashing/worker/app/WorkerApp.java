cf6a66402a169a53ea0bdcb9fe727a26085d9fd6
Martin Graversen <martin@graversen.io>
2019-03-17 16:35:52 +0100

    worker-app application setup

--- worker-app/src/main/java/io/graversen/distributed/hashing/worker/app/WorkerApp.java
+++ worker-app/src/main/java/io/graversen/distributed/hashing/worker/app/WorkerApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.graversen.distributed.hashing.worker.configuration")
 public class WorkerApp
 {
     public static void main(String[] args)
---
+++ worker-app/src/main/java/io/graversen/distributed/hashing/worker/app/configuration/ApplicationConfiguration.java
@@ -0,0 +1,27 @@
+package io.graversen.distributed.hashing.worker.app.configuration;
+
+import io.graversen.trunk.hashing.DigestUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+@Configuration
+@RequiredArgsConstructor
+@ComponentScan(basePackages = {"io.graversen.distributed.hashing.worker.app.service"})
+public class ApplicationConfiguration
+{
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady()
+    {
+        System.out.println("ApplicationConfiguration.onApplicationReady");
+    }
+
+    @Bean
+    public DigestUtils digestUtils()
+    {
+        return new DigestUtils();
+    }
+}
